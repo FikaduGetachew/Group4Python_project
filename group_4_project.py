b6200c602519467b7628aad1ed28907bded34f9c
FikaduGetachew <164221046+FikaduGetachew@users.noreply.github.com>
2024-04-11 10:37:56 -0700

    Update group_4_project.py

--- group_4_project.py
+++ group_4_project.py
@@ -1,11 +1,12 @@
-import sys
 
 #Function use to register studentID,studentName,studentGpa,studentDep,studentGender
 def register_student(info_List):   
    try:
-      print("Registration form")
+      print("**************************")
+      print("*    Registration form   *")
+      print("**************************")
       num_students = int(input("Enter the number of students: "))
-      for i in range(1, num_students + 1):
+      for i in range(num_students ):
             info_List=[]
             with open("student_db.txt", 'r') as file:
                 for line in file:
@@ -13,79 +14,59 @@ def register_student(info_List):
                 st_id = input("Enter student id: ")
                 for studentId in info_List:
                    while st_id== studentId[0]:
-                     print("The ID is already exist enter new ID")
+                     print("The ID is already taken. please enter a new ID")
                      st_id = input("Enter student id: ")
                 st_name = input("Enter student name: ")
                 while st_name>='0'and st_name<='9':
-                    print("enter the Correct  name")
+                    print("Please enter a valid name")
                     st_name = input("Enter student name: ")
-                st_gpa = input("Enter student GPA: ")
-                while st_gpa >'4'or st_gpa<'0':
-                    print("please enter the Correct  gpa")
+                st_gpa = input("Enter student GPA(0.0 - 4.0): ")
+                while st_gpa >'4.0'or st_gpa<'0.0':
+                    print(" please enter the Correct  gpa between 0.0 and 4.0.")
                     st_gpa = input("Enter student gpa: ")
                 st_dep = input("Enter the department: ")
                 while  st_dep>='0'and st_dep<='9':
-                    print("enter the Correct  department")
+                    print("Please enter a valid department name.")
                     st_dep = input("Enter student department: ")
-                st_gen = input("Enter student gender M or F: ")
+                st_gen = input("Enter student gender (M/F): ")
                 while st_gen not in['M','F'] or len(st_gen)!=1:
-                    print("please enter the Correct  gender")
+                    print("Please enter 'M' for Male or 'F' for Female. ")
                     st_gen= input("Enter student gender: ")
                 with open("student_db.txt", "a") as newfile:
                     newfile.write(f"{st_id};{st_name};{st_gpa};{st_dep};{st_gen}\n")
-                    print("Registered successfully")
+                    print("Student Registered successfully!")
       file.close()
    except ValueError:
-       print("Please enter valid number of student")
+       print("Please enter valid number of student.")
    except IOError:
-       print("error accure while accessing file")
+       print("An error accure while accessing file.")
    except Exception as e:
-       print("error",str(e))  
+       print("Oops! Something went wrong:",str(e))  
    finally:              
      menu()
-     
-def show_all(info_List):
-   print("Student database:")
-   print("*********All students found*********")
-   try:
-      
-      with open("student_db.txt", 'r') as file:
-         for line in file:
-             student=line.split(";")
-             if len(student)>=5:
-                 
-                 print(f" ID: {student[0]}\n Name: {student[1]}\n Gpa: {student[2]}\n Department: {student[3]}\n Gender: {student[4]}")
-      print("*********All students found*********")       
-   except FileNotFoundError:
-     print("File not found")
-   except Exception as e:
-      print("error",e)
-   menu()    
-                 
-  
 #Function use search student we want to see
 def search_student(info_List):
   try:
       found = False
-      id_search = input("Enter student ID to search: ")
+      id_search = input("Please Enter student ID to search: ")
       with open("student_db.txt", 'r+') as file:
         for line in file:
             student= line.split(";")
             if student[0] == id_search:
                 print("******** Student found ************")
-                print(f"Name:{student[1]}\nGPA:{student[2]}\nDepartment:{student[3]}\nGender:{student[4]}")
+                print(f"Name: {student[1]}\nGPA: {student[2]}\nDepartment: {student[3]}\nGender: {student[4]}")
                 print("************************************")
                 found = True
                 break
       if not found:
-         print("Student does not exist")
+         print("The Student does not exist")
   except FileNotFoundError:
-      print("File not found")
+      print("The File was not found")
   except Exception as e:
       print("error",e)
   finally:              
      menu()
-#Function use to update student gpa
+#Function used to update student GPA
 def update_gpa(info_List):
   try:
      st_id = input("Enter student ID: ")
@@ -95,10 +76,10 @@ def update_gpa(info_List):
         for line in file:
             student= line.split(";")
             if st_id == student[0]:
-                new_gpa = input("Enter new student GPA: ")
+                new_gpa = input("Enter the new student GPA: ")
                 while new_gpa >'4'or new_gpa<'0' :
-                        print("Please enter the Correct  gpa...")
-                        new_gpa = input("Enter student gpa: ")       
+                        print("Please enter the Correct  GPA...")
+                        new_gpa = input("Enter student's GPA: ")       
                 student[2]=new_gpa
                 line=";".join(student)
                 updated = True
@@ -115,7 +96,24 @@ def update_gpa(info_List):
        print("error",e) 
   finally:       
      menu()
-
+#Function use to display all students we register
+def show_all(info_List):
+   try:
+      print("Student database:")
+      with open("student_db.txt", 'r') as file:
+         for line in file:
+             student=line.split(";")
+             if len(student)>=5:
+                 print("*********All students found*********")
+                 print(f" ID: {student[0]}\n Name: {student[1]}\n Gpa: {student[2]}\n Department: {student[3]}\n Gender: {student[4]}")
+                 print("************************************")       
+   except FileNotFoundError:
+     print("File not found")
+   except Exception as e:
+      print("error",e)
+   menu()    
+                 
+  
 #Function use to update  student name
 def update_name(info_List):
    try:
@@ -196,16 +194,16 @@ def count_students_gender(info_List):
             if len(student)>=5:
              info_List.append(student)
 
-      malecount = 0
-      femalecount = 0
+      mcount = 0
+      fcount = 0
       for student in info_List:
         if len(student)>=5:
          if student[4] == "M":
-            malecount += 1
+            mcount += 1
          elif student[4] == "F":
-            femalecount += 1
-      print(f"Total number of male students: {malecount}")
-      print(f"Total number of female students: {femalecount}")
+            fcount += 1
+      print(f"Total number of male students: {mcount}")
+      print(f"Total number of female students: {fcount}")
    except FileNotFoundError:
        print("File not found")
    except Exception as e:
@@ -265,11 +263,12 @@ def gpa_threshold(info_list):
    try:
      with open("student_db.txt", "r") as file:
         st_gpa = input("Enter the gpa: ")
+        print(f"Top scoring students with a GPA greater than {st_gpa}:\n")
         for line in file:
             student=line.split(";")
-            if len(student)>=4:
-             if float(student[2]) > float(st_gpa) :
-              print(f"Top scoring students that greater than {st_gpa} are {student[1]} from {student[3]} departement with gpa {student[2]}\n")
+            if len(student)>=4 and float(student[2]) > float(st_gpa) :
+              print(f" - Name {student[1]} | Departement{student[3]}  | GPA {student[2]}\n")
+        print()     
    except FileNotFoundError:
       print("File not found")
    except Exception as e:
@@ -289,7 +288,7 @@ def frequent_name(info_List):
              else:
                student_frequency[student[1]] = 1
         for student[1],count in student_frequency.items():    
-            print( f"{student[1]} is registered {count} times\n" )
+            print( f"The Name '{student[1]}' is registered {count} times\n" )
    except FileNotFoundError:
       print("file not found")
    except Exception as e:
@@ -318,33 +317,49 @@ def students_per_department(info_List):
      menu()  
         
        
+# Function to close the System
+def Exit():
+    print("Exiting the system. Thank you for using our service!")
+    SystemExit
 
-# Function to display the menu and handle user choices
+
+# Function to display the menu and Manage the user Selection
 def menu():
-    print("*************************************WELCOME TO STUDENT MANAGEMENT SYSTEM*************************************")
-    print("select your choice")
-    print("0. Registration")
-    print("1. Search Student")
-    print("2. Update Student Name")
-    print("3. Update Student Gpa")
-    print("4. Display All students")
-    print("5. Delete Student Information")
-    print("6. Total Number of Students ")
-    print("7. Total Number of Students based on Gender ")
-    print("8. Top Scoring Students ")
-    print("9. Top Scoring Female Students ")
-    print("10. Search Students based on GPA ")
-    print("11. Display Frequent Student Name ")
-    print("12. Display Student per Department ")
-    print("13. EXIT")
+    print("**************************************************************************")
+    print(        "\t\tWELCOME TO THE STUDENT MANAGEMENT SYSTEM!")
+    print("**************************************************************************\n")
+    print("***","Please choose an option from the following list: \n")
+    print("***","0. Registration")
+    print("***","1. Search Student")
+    print("***","2. Update Student Name")
+    print("***","3. Update Student Gpa")
+    print("***","4. Display All students")
+    print("***","5. Delete Student Information")
+    print("***","6. Total Number of Students ")
+    print("***","7. Total Number of Students based on Gender ")
+    print("***","8. Top Scoring Students ")
+    print("***","9. Top Scoring Women Students ")
+    print("***","10. Search Students based on GPA ")
+    print("***","11. Display Frequent Student Name ")
+    print("***","12. Display Student per Department ")
+    print("***","13. EXIT")
     print("****************************************************************************************************")
     info_List=[]
-    choice = int(input("\n Enter your choice: "))
+    while True:
+        try:
+            choice = int(input("\n Enter your choice: "))
+            if 0<= choice <= 13:
+                break
+            else:
+                print("Invalid input. Please enter a number between 0 and 13.")
+        except ValueError:
+          print("Invalid input. Please enter a number.")
+         
     if choice==0:
         register_student(info_List)
     elif choice==1:
         search_student(info_List)
-    elif choice==2:
+    elif choice==2:  
         update_name(info_List)  
     elif choice==3:
         update_gpa(info_List)
@@ -367,13 +382,9 @@ def menu():
     elif choice==12:
         students_per_department(info_List)
     elif choice==13:
-        sys.exit()
-    else:
-        print("invalid input")                                                          
-    menu()   
-# Function to close the System
-def Exit():
-    exit()     
-
+        Exit()
+        print("invalid input. Please enter a number between 0 and 13.")                                                          
+    menu()
 if __name__ == "__main__":
     menu()
+
